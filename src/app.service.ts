43f91d5660be17f190e3972d5faa8b5a2d58569f
Guapura89 <betocrack1228@gmail.com>
2024-01-31 01:15:58 -0700

    setting vercel.json to deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,12 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  hello = [
-    {
-      title: ' hola',
-      desc: 'perrito',
-    },
-  ];
   getHello(): string {
     return 'Hello World!';
   }
